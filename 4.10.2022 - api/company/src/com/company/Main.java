67516fcfb0777fc4639ae31a50ff254c5f393e91
ksmogorzewski <krzysztofsmogorzewski50@gmail.com>
2022-10-11 09:13:59 +0200

    nwm2

--- 4.10.2022 - api/company/src/com/company/Main.java
+++ 4.10.2022 - api/company/src/com/company/Main.java
@@ -2,7 +2,14 @@ package com.company;
 
 public class Main {
     public static void main(String[] args){
-        User user1 = new User();
-        System.out.println(user1.name);
+        /*User user1 = new User();
+        System.out.println(user1.name);*/
+        User user2 =new User(1,
+                "Jan1",
+                "Dobrowolski1",
+                "jan1@gmail.com",
+                "1111",
+                1);
+        System.out.println(user2.name);
     }
 }
--- 4.10.2022 - api/company/src/com/company/User.java
+++ 4.10.2022 - api/company/src/com/company/User.java
@@ -1,9 +1,16 @@
 package com.company;
 
 //@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class User {
-    public User(){
-        this.name = "Test";
+    public User(int id, String name, String surname, String email, String password, int role){
+        this.id=id;
+        this.name=name;
+        this.surname=surname;
+        this.email=email;
+        this.password=password;
+        this.role=role;
     }
     private int id;
     public String name;
